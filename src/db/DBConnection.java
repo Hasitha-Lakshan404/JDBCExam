4be9a2f043cdc00dc9c2093829370c34d7de2a8a
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-07-12 08:58:23 +0530

    Added DBConnection

---
+++ src/db/DBConnection.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :JDBCExam
+ * Date :7/12/2022
+ * Time :8:55 AM
+ */
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private final Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBCExam", "rooot", "1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return dbConnection == null ? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+
+}
