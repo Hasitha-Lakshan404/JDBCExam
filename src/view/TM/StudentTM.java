f6f67ccdc1c2712dca74b3180de8d5110e4efd40
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-07-12 10:07:25 +0530

    Create StudentTM

---
+++ src/view/TM/StudentTM.java
@@ -0,0 +1,24 @@
+package view.TM;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :JDBCExam
+ * Date :7/12/2022
+ * Time :10:05 AM
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentTM {
+    String studentId;
+    String name;
+    String text;
+    String telNo;
+    String address;
+    String nic;
+}
