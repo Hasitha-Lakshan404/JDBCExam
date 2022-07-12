6582954e57f4a609b9d09e40de9ade14451bc152
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-07-12 10:01:44 +0530

    Update StudentForm Controller

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
 /**
  * @author : Hasitha Lakshan
  * Project :JDBCExam
@@ -9,4 +13,23 @@ package controller;
 
 public class StudentFormController {
 
+    public JFXTextField txtStudentName;
+    public TableView tblStudent;
+    public JFXTextField txtNic;
+    public JFXTextField txtEmail;
+    public JFXTextField txtTellNo;
+    public JFXTextField txtStudentId;
+    public JFXTextField txtAddress;
+
+    public void menuUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAdd(ActionEvent actionEvent) {
+    }
+
+    public void btnClear(ActionEvent actionEvent) {
+    }
 }
