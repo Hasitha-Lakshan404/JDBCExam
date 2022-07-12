f36c848b239ce6a5e273ea3badc7ddf3961814ca
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-07-12 10:10:43 +0530

    Added Table genorics and initialize for stController

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -3,6 +3,7 @@ package controller;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
+import view.TM.StudentTM;
 
 /**
  * @author : Hasitha Lakshan
@@ -14,12 +15,16 @@ import javafx.scene.control.TableView;
 public class StudentFormController {
 
     public JFXTextField txtStudentName;
-    public TableView tblStudent;
+    public TableView<StudentTM> tblStudent;
     public JFXTextField txtNic;
     public JFXTextField txtEmail;
     public JFXTextField txtTellNo;
     public JFXTextField txtStudentId;
     public JFXTextField txtAddress;
+
+    public void initialize(){
+
+    }
 
     public void menuUpdateOnAction(ActionEvent actionEvent) {
     }
